66fd6b6d0639af4d0ed8d6b05ae1d559e13e05de
biglindar <126753730+biglindar@users.noreply.github.com>
2025-05-15 02:36:27 +0800

    Update check.js

--- check.js
+++ check.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const https = require("https");
 
 const urls = [
-  { name: "#us", url: process.env.URL_US },
-  { name: "#in", url: process.env.URL_IN }
+  { name: "#us", url: "https://sub.doon.eu.org/webhostmost-us" },
+  { name: "#in", url: "https://sub.doon.eu.org/webhostmost-in" }
 ];
 
 function formatBeijingTime(date) {
